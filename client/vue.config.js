f863559baaad19715d3da65912cd462fe8a45fed
longfei18 <617903093@qq.com>
2022-05-25 16:39:51 +0800

    Vue+Express前后端分离响应

---
+++ client/vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service')
+
+
+module.exports = {
+  transpileDependencies: true,
+  lintOnSave:false,
+  devServer:{
+    proxy:'http://localhost:3000'
+  }
+}
+
+
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
